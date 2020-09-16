a7e8e47927bea8b913b13f553db5677e7ab9d47d
HwangYoungHa <59431258+HwangYoungHa@users.noreply.github.com>
2020-09-16 14:16:39 +0900

    Add files via upload

---
+++ practice.cpp
@@ -0,0 +1,32 @@
+#include <cstdio>
+#include <cmath>
+
+// cout, cin보다 printf, scanf가 더 빠르다! 
+
+int prime(int n){
+	if(n<=1)
+		return 0;
+	for(int i=2; i<=sqrt(n); i++){
+		if(n%i == 0)
+			return 0;
+	}	
+	return 1;
+}
+
+int main(){
+	int t, n, ans;
+	
+	while(1){
+		ans=0;
+		scanf("%d", &n);
+		if(n==0)
+			break;
+		for(int i=(n+1); i<=2*n; i++){
+			if(prime(i)){
+				ans+=1;
+			}
+		}
+		printf("%d\n", ans);
+	}
+	return 0;	
+}
