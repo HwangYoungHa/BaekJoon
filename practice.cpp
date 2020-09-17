d1ce5f231af926cbd5b35bdf7dbe73cdc190ed7c
HwangYoungHa <59431258+HwangYoungHa@users.noreply.github.com>
2020-09-17 09:18:15 +0900

    Add files via upload

---
+++ practice.cpp
@@ -0,0 +1,21 @@
+#include <cstdio>
+#include <cmath>
+#define _USE_MATH_DEFINES // 3.141592 사용시 틀림.... 
+
+using namespace std;
+
+double uclid(double n){
+	return M_PI * n * n;
+}
+
+double geometry(double n){
+	return 2 * n * n;
+}
+
+int main(){
+	double r;
+	scanf("%lf", &r);
+	printf("%.6f\n", uclid(r));
+	printf("%.6f", geometry(r));
+	return 0;
+}
